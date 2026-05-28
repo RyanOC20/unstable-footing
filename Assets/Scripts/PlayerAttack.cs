ffb8bfb8293df33d0f789c802009f009b87fd10b
RyanOC20 <ryoc20@gmail.com>
2026-05-28 14:05:26 -0700

    Added player attack script.

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5579516bb5445ef95c734eed1b03e59399740bd0bd7b121a5860dbb82a69a560
+size 1578
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84660e2f0b1275c6a5c942468d883630db77dd1e17f908e14b3fbd5372345a49
-size 1609
+oid sha256:9474ffcd74942537c5627d4ef8b8bdc645616888fa0bbed60d0d69445449bba1
+size 2086
