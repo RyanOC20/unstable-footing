91baf1b67e84f41c50a79d7b91e789866c1e8b34
Steven Luong <stevenluong05@gmail.com>
2026-06-09 17:52:37 -0700

    made it so that you can host again after playing a round and going back to main menu

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:117a16b39a4840812561bbbcf985b3c7a5351192ec65727b92c4bccee6f92ec3
-size 661
+oid sha256:6212e1dd3295be966489b765e457196a19023bc1b6318df63dc13cf5130657da
+size 1261
