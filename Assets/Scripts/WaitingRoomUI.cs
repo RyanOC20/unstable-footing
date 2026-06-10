01c34e6e55b458b415f84e4122aef04bb3d5fa6d
Steven Luong <stevenluong05@gmail.com>
2026-06-09 18:12:16 -0700

    renamed sample scene to arena with references

--- Assets/Scripts/WaitingRoomUI.cs
+++ Assets/Scripts/WaitingRoomUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:546bab0cc57499c8d17387326bbe9d908940d5a08b461155c1d952494f2a354a
-size 2884
+oid sha256:4019df6bc476657f3510dfa62d1392136c7e5b2eedae01e67b39cd9ad344f298
+size 2878
