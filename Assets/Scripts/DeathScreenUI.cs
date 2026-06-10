f72a4d69474864aa5f2a02f1a5adf21a7923cb95
Steven Luong <stevenluong05@gmail.com>
2026-06-09 21:19:17 -0700

    added a death screen

---
+++ Assets/Scripts/DeathScreenUI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84238076b2c4402854547b714fe5201cd5b1645d40531ef219b028cca81631c3
+size 1011
--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95c7311fa643e244065c67ad56a15fd7e5fc8d891ce386391b3cfe0530bb831b
-size 1425
+oid sha256:1b8182d971656138a8e4d5f88b97b11b2f095808ec2537e1e9e4548d9553f4f3
+size 2040
