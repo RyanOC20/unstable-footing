7f2fa2cdb23e4688da578a0625d06f7deca3b48f
Steven Luong <stevenluong05@gmail.com>
2026-06-09 19:31:50 -0700

    integrated arena into game

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42b5abf14fcbc9b06cbe8d2c1625e053c6884abcc488a34e91aaadd2e972f8fe
-size 1490
+oid sha256:95c7311fa643e244065c67ad56a15fd7e5fc8d891ce386391b3cfe0530bb831b
+size 1425
